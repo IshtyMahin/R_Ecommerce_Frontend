e7c929c5631d0d68615afbeaf5cbf567286ba70a
IshtyMahin <ishty119@gmail.com>
2025-04-19 17:59:48 +0600

    add suspense in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,12 @@
-
-"use client";
 import LoginForm from "@/components/modules/auth/login/LoginForm";
+import { Suspense } from "react";
 
 const LoginPage = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <LoginForm />
+      <Suspense fallback={<div>Loading login form...</div>}>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 };
